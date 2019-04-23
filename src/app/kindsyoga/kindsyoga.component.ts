7b72c646d7e99d9aaab768fddafdd6850f98d6ac
Eddie <eddie@edease.nl>
2019-04-23 16:10:08 +0200

    justupdating

--- src/app/kindsyoga/kindsyoga.component.ts
+++ src/app/kindsyoga/kindsyoga.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+
+import {
+  BsModalService
+} from 'ngx-bootstrap/modal';
+import {
+  BsModalRef
+} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
 
 @Component({
   selector: 'app-kindsyoga',
@@ -7,10 +15,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KindsyogaComponent implements OnInit {
 
-  constructor() { }
+    // reference of modal
+    modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
+  }
+
+  // opening the modal
+  openModal(template: TemplateRef < any > ) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  closeModalNow(){
+    this.modalRef.hide();
   }
 
 }
